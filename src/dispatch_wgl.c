cf65cdd63975e8f384aa113001afc75f934931c2
Chun-wei Fan <fanchunwei@src.gnome.org>
2014-10-23 19:35:32 +0800

    Non-generated Sources: Allow Build on Visual Studio
    As this code is obviously not only built on gcc, define a constructor that
    is suitable for use on Visual Studio, in addition to the current gcc one.
    
    Also, avoid defining an empty struct (which is not allowed on any MSVC),
    and make sure that we have GLAPIENTRY (i.e. __stdcall) just before the
    parameter part of the function declaration/prototype, as that is where
    Visual Studio expects them to be, and is also accepted on gcc.
    
    As epoxy_conservative_gl_version() is marked for export in
    dispatch_common.c, do likewise for distpatch_common.h by using PUBLIC, as
    Visual Studio does not like export annotations to be done on function
    declarations but not the prototypes.

--- src/dispatch_wgl.c
+++ src/dispatch_wgl.c
@@ -142,7 +142,7 @@ DllMain(HINSTANCE dll, DWORD reason, LPVOID reserved)
     return TRUE;
 }
 
-WRAPPER_VISIBILITY BOOL
+WRAPPER_VISIBILITY (BOOL)
 WRAPPER(epoxy_wglMakeCurrent)(HDC hdc, HGLRC hglrc)
 {
     BOOL ret = epoxy_wglMakeCurrent_unwrapped(hdc, hglrc);
@@ -153,7 +153,7 @@ WRAPPER(epoxy_wglMakeCurrent)(HDC hdc, HGLRC hglrc)
 }
 
 
-WRAPPER_VISIBILITY BOOL
+WRAPPER_VISIBILITY (BOOL)
 WRAPPER(epoxy_wglMakeContextCurrentARB)(HDC hDrawDC,
                                         HDC hReadDC,
                                         HGLRC hglrc)
@@ -167,7 +167,7 @@ WRAPPER(epoxy_wglMakeContextCurrentARB)(HDC hDrawDC,
 }
 
 
-WRAPPER_VISIBILITY BOOL
+WRAPPER_VISIBILITY (BOOL)
 WRAPPER(epoxy_wglMakeContextCurrentEXT)(HDC hDrawDC,
                                         HDC hReadDC,
                                         HGLRC hglrc)
@@ -181,7 +181,7 @@ WRAPPER(epoxy_wglMakeContextCurrentEXT)(HDC hDrawDC,
 }
 
 
-WRAPPER_VISIBILITY BOOL
+WRAPPER_VISIBILITY (BOOL)
 WRAPPER(epoxy_wglMakeAssociatedContextCurrentAMD)(HGLRC hglrc)
 {
     BOOL ret = epoxy_wglMakeAssociatedContextCurrentAMD_unwrapped(hglrc);
