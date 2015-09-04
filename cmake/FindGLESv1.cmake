2af1715fbf5e7a9508bec01e70ec6bba8ec5204b
Yaron Cohen-Tal <yaronct@gmail.com>
2015-09-04 12:51:13 +0300

    Fix "FindGLESv1.cmake" and "FindGLESv2.cmake".

--- cmake/FindGLESv1.cmake
+++ cmake/FindGLESv1.cmake
@@ -1,12 +1,12 @@
 # Find GLESv1
 #
 # GLESv1_LIBRARY
-# GLESv1_FOUND
+# GLES_V1_FOUND
 
-set(GLESv1_NAMES ${GLESv1_NAMES} GLESv1_CM libGLES_CM)
+set(GLESv1_NAMES ${GLESv1_NAMES} GLES_CM GLESv1_CM libGLES_CM)
 find_library (GLESv1_LIBRARY NAMES ${GLESv1_NAMES} PATHS /opt/vc/lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args (GLESv1 DEFAULT_MSG GLESv1_LIBRARY)
+find_package_handle_standard_args (GLES_V1 DEFAULT_MSG GLESv1_LIBRARY)
 
 mark_as_advanced (GLESv1_LIBRARY)
