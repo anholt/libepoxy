0f7f16b7320fd19480ad1519346354c8b52662a3
Emmanuele Bassi <ebassi@gnome.org>
2016-12-07 16:13:09 +0000

    build: Protect paths from spaces
    Completes commit 83cfa6a1b5b665ec99f5bbd44650b4816ba8fef5.

--- autogen.sh
+++ autogen.sh
@@ -4,11 +4,11 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
-cd $srcdir
+cd "$srcdir"
 
 autoreconf -v --install || exit 1
 cd "$ORIGDIR" || exit $?
 
 if test -z "$NOCONFIGURE"; then
-    $srcdir/configure "$@"
+    "$srcdir/configure" "$@"
 fi
